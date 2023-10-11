0d26cc4e7677e513e5e08a7f062156fec3ada632
Marcel <marcel202101@gmail.com>
2023-10-10 23:47:05 -0400

    Completed

---
+++ hack_9.py
@@ -0,0 +1,14 @@
+"""
+loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
+"""
+
+def fn_hack_9():
+    result = [1,2,3]
+    new_result =[]
+    for i in result:
+        new_result.append(i)
+        new_result.append("@")
+    #...
+    return new_result
+
+print(fn_hack_9())
